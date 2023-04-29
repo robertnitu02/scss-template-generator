42acbcc28eaa9a2ce69184b1e033de8e46659fb4
Robert Nițu <niturobert02@yahoo.com>
2023-04-29 14:55:08 +0300

    Added ngx translate

--- template-generator/src/app/app.component.ts
+++ template-generator/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TranslateService } from '@ngx-translate/core';
+import defaultEnLanguage from '../shared/i18n/en.json';
+import defaultRoLanguage from '../shared/i18n/ro.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'template-generator';
+
+  constructor(private translate: TranslateService) {
+    translate.setTranslation('en', defaultEnLanguage);
+    translate.setTranslation('ro', defaultRoLanguage);
+    translate.setDefaultLang('en');
+  }
+
+  ngOnInit(): void {
+    console.log(1);
+  }
 }
--- template-generator/src/app/app.module.ts
+++ template-generator/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// import ngx-translate and the http loader
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
