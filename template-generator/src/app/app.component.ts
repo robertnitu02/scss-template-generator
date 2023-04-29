9e206d98b9f37e9becf4d198812cefe4ff67a29e
Robert Nițu <niturobert02@yahoo.com>
2023-04-29 17:58:46 +0300

    Added content page

--- template-generator/src/app/app.component.ts
+++ template-generator/src/app/app.component.ts
@@ -4,13 +4,22 @@ import { TranslateService } from '@ngx-translate/core';
 import defaultEnLanguage from '../shared/i18n/en.json';
 import defaultRoLanguage from '../shared/i18n/ro.json';
 
+enum ViewState {
+  HOME,
+  FORM,
+  RESULT,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  viewState = ViewState;
+
   language = 'en';
+  currentViewState = this.viewState.HOME;
 
   constructor(private translate: TranslateService) {
     translate.setTranslation('en', defaultEnLanguage);
@@ -18,12 +27,19 @@ export class AppComponent implements OnInit {
     translate.setDefaultLang(this.language);
   }
 
-  ngOnInit(): void {
-    console.log(1);
-  }
+  ngOnInit(): void {}
 
   changeLanguage() {
     this.language = this.language == 'ro' ? 'en' : 'ro';
     this.translate.use(this.language);
+  }
+
+  goHome() {
+    if (this.currentViewState === this.viewState.HOME) return;
+    this.currentViewState = this.viewState.HOME;
+  }
+
+  createTemplate() {
+    this.currentViewState = this.viewState.FORM;
   }
 }
