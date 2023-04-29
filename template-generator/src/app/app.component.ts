ed5bb38891d5beb9754f35b391023769fff61db9
Robert Nițu <niturobert02@yahoo.com>
2023-04-29 15:32:49 +0300

    Added ngx translate. Added navbar

--- template-generator/src/app/app.component.ts
+++ template-generator/src/app/app.component.ts
@@ -10,15 +10,20 @@ import defaultRoLanguage from '../shared/i18n/ro.json';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'template-generator';
+  language = 'en';
 
   constructor(private translate: TranslateService) {
     translate.setTranslation('en', defaultEnLanguage);
     translate.setTranslation('ro', defaultRoLanguage);
-    translate.setDefaultLang('en');
+    translate.setDefaultLang(this.language);
   }
 
   ngOnInit(): void {
     console.log(1);
+  }
+
+  changeLanguage() {
+    this.language = this.language == 'ro' ? 'en' : 'ro';
+    this.translate.use(this.language);
   }
 }
