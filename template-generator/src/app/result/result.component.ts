3eef6a054728f50aeb19eb6ea59b40fc1c41a91a
Robert Nițu <niturobert02@yahoo.com>
2023-04-30 18:08:36 +0300

    Wip

--- template-generator/src/app/result/result.component.ts
+++ template-generator/src/app/result/result.component.ts
@@ -7,7 +7,47 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
   @Input() results: number[] = [];
+  source: string = `
+  import { Component } from '@angular/core';
 
+  @Component({
+    template: 'Hello {{ name }}'
+  })
+  class HelloWorldComponent {
+    name: string = 'World!';
+  }
+    import { Component } from '@angular/core';
+
+  @Component({
+    template: 'Hello {{ name }}'
+  })
+  class HelloWorldComponent {
+    name: string = 'World!';
+  }
+    import { Component } from '@angular/core';
+
+  @Component({
+    template: 'Hello {{ name }}'
+  })
+  class HelloWorldComponent {
+    name: string = 'World!';
+  }
+    import { Component } from '@angular/core';
+
+  @Component({
+    template: 'Hello {{ name }}'
+  })
+  class HelloWorldComponent {
+    name: string = 'World!';
+  }
+    import { Component } from '@angular/core';
+
+  @Component({
+    template: 'Hello {{ name }}'
+  })
+  class HelloWorldComponent {
+    name: string = 'World!';
+  }`;
   ngOnInit(): void {
     console.log(this.results);
   }
