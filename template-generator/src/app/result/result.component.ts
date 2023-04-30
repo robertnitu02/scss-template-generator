2c73a6eed9cce8d50ae56881b38dfaec0016521a
Robert Nițu <niturobert02@yahoo.com>
2023-04-30 16:36:22 +0300

    Added style for result component. Added copy to clipboard

--- template-generator/src/app/result/result.component.ts
+++ template-generator/src/app/result/result.component.ts
@@ -7,6 +7,81 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ResultComponent implements OnInit {
   @Input() results: number[] = [];
+  code = `.wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container {
+    flex: 1;
+    margin: 1em;
+    position: relative;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container {
+    flex: 1;
+    margin: 1em;
+    position: relative;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container {
+    flex: 1;
+    margin: 1em;
+    position: relative;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container {
+    flex: 1;
+    margin: 1em;
+    position: relative;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .wrapper {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .container {
+    flex: 1;
+    margin: 1em;
+    position: relative;
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }`;
 
   ngOnInit(): void {
     console.log(this.results);
