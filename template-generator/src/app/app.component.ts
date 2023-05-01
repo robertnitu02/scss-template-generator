6630bd13b873d95292834b47575884038170c366
Robert Nițu <niturobert02@yahoo.com>
2023-05-02 01:26:48 +0300

    Fix goBack functionality

--- template-generator/src/app/app.component.ts
+++ template-generator/src/app/app.component.ts
@@ -58,7 +58,7 @@ export class AppComponent implements OnInit {
 
   goBack() {
     if (
-      this.currentQuestionIndex < 0 ||
+      this.currentQuestionIndex === 0 ||
       this.currentQuestionIndex > this.maxQuestions
     )
       return;
